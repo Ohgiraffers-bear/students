16a6db8551e3998d3538bec9af623a7d56838610
Ohgiraffers-bear <bear.ohgiraffers@gmail.com>
2025-04-10 13:16:11 +0900

    first commit

---
+++ src/api/axios.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+// axios 인스턴스 생성
+const apiClient = axios.create({
+  baseURL: "https://kb-json.onrender.com",
+  timeout: 5000, // 5초
+});
+
+export default apiClient;
