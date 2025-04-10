68be32c76d417652ea2ab7771663635f378837c8
Ohgiraffers-bear <bear.ohgiraffers@gmail.com>
2025-04-10 13:32:46 +0900

    baseUrl 수정

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // axios 인스턴스 생성
 const apiClient = axios.create({
-  baseURL: "https://kb-json.onrender.com",
+  baseURL: "https://paint-speckle-kumquat.glitch.me",
   timeout: 5000, // 5초
 });
 
